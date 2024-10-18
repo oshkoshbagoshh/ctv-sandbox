5a3a25b723ecaaa8536385aa1c0b94132029f2a5
AJ Javadi <amirjavadi25@gmail.com>
2024-10-18 10:33:18 -0500

    redoing the schemas

--- my-ctv-consulting-firm/resources/views/subscription-confirmation.blade.php
+++ my-ctv-consulting-firm/resources/views/subscription-confirmation.blade.php
@@ -7,7 +7,7 @@
 <h1>Welcome to our CTV Newsletter, {{ $subscriberName }}!</h1>
 <p>Thank you for subscribing to our newsletter. We're excited to keep you updated on the latest CTV trends and
     insights. </p>
-<p>Your subscription email: {{ $subscripterEmail }}</p>
+<p>Your subscription email: {{ $subscriberEmail }}</p>
 <p>If you have any questions, feel free to contact us.</p>
 <p>Best regards,<br>Your CTV Consulting Team</p>
 </body>
