f02f0bf45d14667b263651f970fec6b210872ea0
AJ Javadi <amirjavadi25@gmail.com>
2024-10-18 12:23:58 -0500

    sub controller

--- my-ctv-consulting-firm/app/Http/Controllers/SubscriptionController.php
+++ my-ctv-consulting-firm/app/Http/Controllers/SubscriptionController.php
@@ -2,60 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SubscriptionConfirmation;
 
 class SubscriptionController extends Controller
 {
-    // subscribe function to validate emails
-
     public function subscribe(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:subscribers,email',
+        ]);
+
+        try {
+            // Save the subscriber to the database
+            $subscriber = Subscriber::create([
+                'email' => $validatedData['email'],
+                'first_name' => explode(' ', $validatedData['name'])[0], // Assuming the first word is the first name
+                'last_name' => substr(strstr($validatedData['name'], ' '), 1), // Get everything after the first space
+                'status' => 'active',
             ]);
 
-        // Save the subscriber to your database here
-        // ...
-        //
-        //namespace App\Http\Controllers;
-        //
-        //use Illuminate\Http\Request;
-        //use Illuminate\Support\Facades\Mail;
-        //use App\Mail\SubscriptionConfirmation;
-        //
-        //class SubscriptionController extends Controller
-        //{
-        //    // subscribe function to validate emails
-        //
-        //    public function subscribe(Request $request)
-        //    {
-        //        $request->validate([
-        //            'name' => 'required|string|max:255',
-        //            'email' => 'required|email|unique:subscribers,email',
-        //            ]);
-        //
-        //        // Save the subscriber to your database here
-        //        // ...
-        //        //TODO:
-        //
-        //        // Send confirmation email
-        //        Mail::to($request->email)->send(new SubscriptionConfirmation($request->name, $request->email));
-        //
-        //        return redirect()->back()->with('success', 'Thank you for subscribing! Please check your email for confirmation');
-        //
-        //    }
-        //
-        //}
-        //TODO:
+            // Send confirmation email
+            Mail::to($subscriber->email)->send(new SubscriptionConfirmation($subscriber->first_name, $subscriber->email));
 
-        // Send confirmation email
-        Mail::to($request->email)->send(new SubscriptionConfirmation($request->name, $request->email));
+            return redirect()->back()->with('success', 'Thank you for subscribing! Please check your email for confirmation.');
+        } catch (\Exception $e) {
+            // Log the error
+            \Log::error('Subscription error: ' . $e->getMessage());
 
-        return redirect()->back()->with('success', 'Thank you for subscribing! Please check your email for confirmation');
-
+            return redirect()->back()->with('error', 'An error occurred while processing your subscription. Please try again later.');
+        }
     }
-
 }
