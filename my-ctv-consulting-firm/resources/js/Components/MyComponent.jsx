004bb31de91f8d4262d1ad33c4175b8be354dbcc
AJ Javadi <amirjavadi25@gmail.com>
2024-10-18 00:52:04 -0500

    finished innitial React and frontend setup

---
+++ my-ctv-consulting-firm/resources/js/Components/MyComponent.jsx
@@ -0,0 +1,11 @@
+function MyComponent() {
+    return (
+        <div className={"bg-blue-500 text-white p-4 rounded-lg "}>
+
+            Hello, Mister!
+
+        </div>
+    );
+}
+
+export default  MyComponent;
--- my-ctv-consulting-firm/resources/js/app.js
+++ my-ctv-consulting-firm/resources/js/app.js
@@ -1,13 +1,14 @@
-// import './bootstrap';
+import './bootstrap';
+import '../css/app.css';
+import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {createInertiaApp} from "@inertiajs/react";
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', {eager: true})
-        return pages[`./Pages/${name}.jsx`]
-    },
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({el, App, props}) {
-        createRoot(el).render(<App{...props} />)
+        createRoot(el).render(<App {...props} />);
     },
-})
+});
